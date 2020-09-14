b1bcaeafca4737249cb2e12a6048034b8581c841
Wilberger Matias <matias180399@gmail.com>
2020-09-14 15:44:25 -0300

    commit conflicto

--- tester.java
+++ tester.java
@@ -2,7 +2,7 @@ package para_repositorio;
 
 public class tester {
 	public static void main(String [] args) {
-		Humano h = new Humano("jose", 41858652);
+		Humano h = new Humano("MATIAS", 41858652);
 		System.out.println(h.getDNI());
 	}
 }
